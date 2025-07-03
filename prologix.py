4f06f49656e647e4c104b7950893e2ce64b21ad4
xenia <xenia.agda@gmail.com>
2025-07-03 20:23:30 +0200

    fix prologix impl. everything else's broken lmao

--- prologix.py
+++ prologix.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Optional
 import socket
 import datetime
@@ -7,7 +8,8 @@ from commlog import CommLog
 
 # Prologix GPIB-Ethernet bridge communication layer
 
-TIMEOUT = 2
+TIMEOUT = 0.1
+EOT_BREAK_CHAR = 123
 
 class Prologix:
     def __init__(
@@ -24,6 +26,12 @@ class Prologix:
         self.log = CommLog(log_path)
         self.sock.send(b"++auto 0\n") # don't auto-receive
         self.sock.send(b"++eos 2\n") # append LF to commands
+
+        # terminate all messages with a null byte
+        # note that a message can contain a null byte already though.
+        # so when we get a null byte we see if receiving another timeouts
+        self.sock.send(b"++eot_enable 1\n")
+        self.sock.send(f"++eot_char {EOT_BREAK_CHAR}\n".encode())
 
         self.sock.settimeout(TIMEOUT)
 
@@ -64,25 +72,91 @@ class Prologix:
     ):
         if self.current_addr != addr:
             self._address_device(addr)
+
+        # if no data has been received yet, ++read will do nothing.
+        # thus we want to continually issue ++read until all data has been read
+        received_data = b''
+        data_hdr = f"Recv from {self.current_addr}"
+        self.sock.send(b'++read eoi\n')
         while True:
             try:
-                self.sock.send(b'++read eoi\n')
-                res = b''
-                while True:
-                    ch = self.sock.recv(1)
-                    self.log.write(f"Recv from {self.current_addr}", ch)
-                    if ch == b'\x1b':
-                        ch = self.sock.recv(1)
-                        self.log.write(f"Recv from {self.current_addr}", ch)
-                    if ch == b'\r':
-                        self.sock.recv(1) # eat the \n
-                        return res
-                    if ch == b'\n':
-                        return res
-                    res += ch
+                ch = self.sock.recv(1)
             except TimeoutError as e:
-                self.log.event("Read failure. Retrying")
-                time.sleep(TIMEOUT)
+                # try to issue another ++read call
+                self.log.event("Timeouted. Issuing another ++read")
+                self.sock.send(b'++read eoi\n')
+                continue
+            self.log.write(data_hdr, ch)
+
+            received_data += ch
+            if ch[0] == EOT_BREAK_CHAR:
+                self.log.event("Potential EOT break")
+                try:
+                    self.sock.settimeout(0.01) # fast timeout
+                    ch = self.sock.recv(1)
+                    self.log.event("Fake EOT, continuing")
+                    received_data += ch
+                    self.log.write(data_hdr, ch)
+                except TimeoutError as e:
+                    # we're properly done
+                    self.log.event("EOT break was real")
+                    return received_data[:-1] # remove EOT_BREAK_CHAR
+                finally:
+                    # reset the timeout
+                    self.sock.settimeout(TIMEOUT)
+
+
+
+
+        # while True:
+        #     self.sock.send(b'++read eoi\n')
+
+        #     while True:
+        #         try:
+        #             ch = self.sock.recv(1)
+        #         except TimeoutError as e:
+        #             self.log.event("Read failure. Retrying")
+        #             time.sleep(TIMEOUT)
+        #             continue
+
+        #         self.log.write(f"Recv from {self.current_addr}", ch)
+        #         if ch[0] == EOT_BREAK_CHAR:
+        #             self.log.event("Got a null byte. Seeing if there's more data")
+
+        #             try:
+        #                 self.sock.settimeout(0.01) # short timeout
+        #                 next_ch = self.sock.recv(1)
+        #                 self.log.write(f"Recv from {self.current_addr}", next_ch)
+        #                 res += ch
+        #                 ch = next_ch
+        #             except TimeoutError as e:
+        #                 # we're done
+        #                 self.log.event("Nope. We timeouted.")
+        #                 return res
+        #             finally:
+        #                 self.sock.settimeout(TIMEOUT) # reset the timeout
+
+        #             self.log.write(f"Recv from {self.current_addr}", ch)
+        #         res += ch
+
+    def device(self, addr: int) -> PrologixDevice:
+        return PrologixDevice(self, addr)
+
+class PrologixDevice:
+    def __init__(self, comm: Prologix, addr: int):
+        self.comm = comm
+        self.addr = addr
+
+    def send_command(self, cmd: bytes):
+        self.comm.send_command(cmd, self.addr)
+
+    def read_until_eoi(self):
+        return self.comm.read_until_eoi(self.addr)
+
+    def query(self, cmd: bytes):
+        self.send_command(cmd)
+        return self.read_until_eoi()
+
 
 if __name__ == "__main__":
     p = Prologix("/tmp/prologix.log", '10.30.42.1', 1234)
